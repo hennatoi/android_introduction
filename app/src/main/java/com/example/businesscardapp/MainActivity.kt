c3d3d32b3f5fe75b7dd02fe7affd37bcaa41fbee
hennatoi <henna_rytilahti@hotmail.com>
2024-03-06 16:42:52 +0200

    Added box composable and improved the layout

--- app/src/main/java/com/example/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -21,14 +21,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscardapp.ui.theme.BusinessCardAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -58,102 +59,116 @@ fun BusinessCardApp() {
             image = painterResource(R.drawable.android_logo),
             phoneImage = painterResource(R.drawable.baseline_phone_black_24),
             shareImage = painterResource(R.drawable.baseline_share_black_18),
+            emailImage = painterResource(R.drawable.baseline_email_black_18),
             name = stringResource(R.string.person_name),
             title = stringResource(R.string.person_title),
             phoneNumber = stringResource(R.string.phone_number),
             contactAddress = stringResource(R.string.contact_address),
+            emailAddress = stringResource(R.string.email_address),
             backgroundColor = colorResource(R.color.green),
-            modifier = Modifier.padding(/*top = 8.dp, bottom = 8.dp*/)
+            fontColor = colorResource(R.color.white),
+            modifier = Modifier.padding()
         )
-        ComposablePhoneNumber(
-            phoneImage = painterResource(R.drawable.android_logo),
-            phoneNumber = stringResource(R.string.phone_number),
-            backgroundColor = colorResource(R.color.green),
-            modifier = Modifier.padding()/*Modifier.weight(1f)*/
-        )
-        /*    ComposableContactInfo(
-                mediaImage = stringResource(R.string.second_title),
-                contactAddress = stringResource(R.string.contact_address),
-                modifier = Modifier.weight(1f)
-            )  */
     }
 }
+
 @Composable
 private fun ComposableInfoCard(
     image: Painter,
     phoneImage: Painter,
     shareImage: Painter,
+    emailImage: Painter,
     name: String,
     title: String,
     phoneNumber: String,
+    emailAddress: String,
     contactAddress: String,
     backgroundColor: Color,
+    fontColor: Color,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .background(backgroundColor)
-            .padding(top = 170.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
     ) {
-        Image(painter = image, contentDescription = null, modifier = Modifier.size(180.dp))
-        Text(
-            text = name,
-            modifier = Modifier.padding(bottom = 16.dp),
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = title,
-            textAlign = TextAlign.Justify
-        )
-        Spacer(Modifier.weight(0.02f))
-        Row {
-            Image(painter = phoneImage, contentDescription = null, modifier = Modifier.size(20.dp))
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(backgroundColor)
+                .padding(top = 170.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(painter = image, contentDescription = null, modifier = Modifier.size(180.dp))
             Text(
-                text = phoneNumber,
-                modifier = Modifier.padding(start = 16.dp, bottom = 16.dp),
-                fontWeight = FontWeight.Bold
+                text = name,
+                modifier = Modifier.padding(bottom = 16.dp),
+                fontWeight = FontWeight.Bold,
+                fontSize = 40.sp,
+                fontFamily = FontFamily.Serif
             )
-        }
-        Row {
-            Image(painter = shareImage, contentDescription = null, modifier = Modifier.size(20.dp))
             Text(
-                text = contactAddress,
-                modifier = Modifier.padding(start = 16.dp, bottom = 16.dp),
-                fontWeight = FontWeight.Bold
+                text = title,
+                textAlign = TextAlign.Justify,
+                color = fontColor,
+                fontSize = 19.sp
             )
+            Spacer(Modifier.weight(0.02f))
         }
+        Column(
+            modifier = Modifier.align(Alignment.BottomCenter)
+                .padding(bottom = 32.dp)
+        ) {
+            Row(
+                modifier = modifier
+                    .padding()
+            ) {
+                Image(
+                    painter = phoneImage,
+                    contentDescription = "Phone number",
+                    modifier = Modifier.size(20.dp)
+                )
+                Text(
+                    text = phoneNumber,
+                    modifier = Modifier.padding(start = 16.dp, bottom = 16.dp),
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Row(
+                modifier = modifier
+                    .padding()
+            ) {
+                Image(
+                    painter = shareImage,
+                    contentDescription = "Share the contact info",
+                    modifier = Modifier.size(20.dp)
+                )
+                Text(
+                    text = contactAddress,
+                    modifier = Modifier.padding(start = 16.dp, bottom = 16.dp),
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Left
+                )
+            }
+            Row(
+                modifier = modifier
+                    .padding()
+            ) {
+                Image(
+                    painter = emailImage,
+                    contentDescription = "E-mail address",
+                    modifier = Modifier.size(20.dp)
+                )
+                Text(
+                    text = emailAddress,
+                    modifier = Modifier.padding(start = 16.dp, bottom = 16.dp),
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Left
+                )
+            }
+        }
+
     }
 }
-
-@Composable
-private fun ComposablePhoneNumber(
-    phoneImage: Painter,
-    phoneNumber: String,
-    backgroundColor: Color,
-    modifier: Modifier = Modifier
-) {
-    Row(
-        modifier = modifier
-            .fillMaxSize()
-            .background(backgroundColor)
-            .padding(top = 150.dp),
-     //   verticalArrangement = Arrangement.Center,
-        //   horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Image(painter = phoneImage, contentDescription = null, modifier = Modifier.size(180.dp))
-        Text(
-            text = phoneNumber,
-            modifier = Modifier.padding(bottom = 16.dp),
-            fontWeight = FontWeight.Bold
-        )
-    }
-
-}
-
-
 @Preview(showBackground = true)
 @Composable
 fun BusinessCardAppPreview() {
